a58faccaf622b7cbc699693b1d45b95676555533
Jake Simms <jakedsimms@gmail.com>
2013-11-06 11:34:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ucanbe::Application.config.secret_key_base = '5f19a410078596c50ab7c6e8bebe665ef9b3371c064fef00b3ff696545446e71a702ebd5e29de23ca9f0d1934e3d0a6e069c5511dab9fdb133fb76c71b97ed8e'

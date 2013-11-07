27ce7d06f609b19fba22ef68f3dcf46bbbae01db
Jake Simms <jakedsimms@gmail.com>
2013-11-07 18:04:02 -0500

    add static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def landing
+  end
+
+  def home
+  end
+
+  def how
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ucanbe::Application.routes.draw do
+  get "static_pages/landing"
+  get "static_pages/home"
+  get "static_pages/how"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

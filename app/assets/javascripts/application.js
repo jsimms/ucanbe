93ae31f6b9279e7701aff04ac2409817461413b9
Jake Simms <jakedsimms@gmail.com>
2013-12-10 14:27:23 -0500

    new animator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.typer.js
+//= require jquery.simple-text-rotator.js
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
